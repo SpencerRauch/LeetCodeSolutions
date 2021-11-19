2bdb04ca20b8135af6410b407cbe8825e64f3dc4
Spencer Rauch <rauchs@gmail.com>
2021-11-18 17:33:05 -0800

    11/19

--- HammingDistance.js
+++ HammingDistance.js
@@ -15,4 +15,20 @@
     
 };
 
+/**
+ * @param {number} x
+ * @param {number} y
+ * @return {number}
+ */
+ var hammingDistance2 = function(x, y) {
+    let hD = 0
+    let diff = x ^ y; //XOR sets 1 in every position where x and y differ
+    while (diff > 0){
+        hD += diff % 2; // if last bit is 1, add 1, if it's 0, add 0!
+        diff = diff >> 1; // shift off last bit
+    }
+    return hD;
+      
+};
+
 // https://leetcode.com/problems/hamming-distance
