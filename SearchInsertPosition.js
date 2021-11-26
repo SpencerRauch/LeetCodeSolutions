33535ec31aa01bc904c89a360daf23ce7823eab3
SpencerRauch <rauchs@gmail.com>
2021-11-26 07:56:23 -0800

    ceil faster than floor

--- SearchInsertPosition.js
+++ SearchInsertPosition.js
@@ -8,7 +8,7 @@ var searchInsert = function(nums, target) {
     let end = nums.length -1;
     
     while (start <= end){
-        let mid = Math.floor((start + end)/2);
+        let mid = Math.ceil((start + end)/2);
         if (nums[mid] < target){
             start = mid +1
         } else end = mid - 1;
