19ed7b14b24c9f4282cb446f320567bcb441a790
Spencer Rauch <rauchs@gmail.com>
2021-12-09 08:34:04 -0800

    12/9

---
+++ JumpGameIII.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} arr
+ * @param {number} start
+ * @return {boolean}
+ */
+var canReach = function(arr, start) {
+    if (arr[start] == 0) return true; // landed on target (base case)
+    if (arr[start] == undefined || arr[start] == -1) return false; //outside of array or visited
+    let left = start + arr[start] //generate our left and right choices for next index
+    let right = start - arr[start] 
+    arr[start] = -1 //mark as visited
+    
+    return canReach(arr, left) || canReach(arr, right) //recursive calls 
+};
