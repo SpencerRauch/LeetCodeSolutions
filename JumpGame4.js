7e9ed023c5ee982591c8412215689f729301512f
Spencer Rauch <rauchs@gmail.com>
2022-01-15 08:16:05 -0800

    comments

--- JumpGame4.js
+++ JumpGame4.js
@@ -13,28 +13,28 @@
     
     let map = {}
     
-    for ( let i = 0; i < n; i++){
+    for ( let i = 0; i < n; i++){ //build a hash map containing an array with every index a value appears at
         if (map[arr[i]]){
             map[arr[i]].push(i)
         } else {
             map[arr[i]] = [i]
         }
     }
-    let indicesToCheck = [0]
+    let indicesToCheck = [0] // initialize our indices to check to the first index
     let jumps = 0
     
     while (indicesToCheck.length > 0){
         let nextToCheck = [];
         let l = indicesToCheck.length
         
-        for (let i = 0; i < l ; i++){
+        for (let i = 0; i < l ; i++){ //iterate through indices to check to see if any of our jumps reached the end
             let currentIdx = indicesToCheck[i]
             
-            if (currentIdx == n -1) {
+            if (currentIdx == n -1) { // we've reached the last index, return amount of jumps
                 return jumps
             }
             
-            let value = arr[currentIdx]
+            let value = arr[currentIdx] //extract value of current index
             arr[currentIdx] = undefined // mark as visited
             
             if (currentIdx + 1 <= n-1 && arr[currentIdx + 1]){ // if next index is in range and not visited
@@ -48,10 +48,10 @@
             if (map[value]){ //if value is in our map, we need to check jumps to all it's indices
                 for (let idx of map[value]){
                     if (arr[idx]){ //checks if index has already been visited
-                        nextToCheck.push(idx)
+                        nextToCheck.push(idx) // if not, put it in our list to check next
                     }
                 }
-                delete map[value]
+                delete map[value] //all indices value appears at will be checked, so we remove it from our map
             }
             
         }
