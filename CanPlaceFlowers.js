1cba83f0b9a29668f6f068a8c0bf5d24897a3bc2
Spencer Rauch <rauchs@gmail.com>
2022-01-18 07:54:41 -0800

    1/18

---
+++ CanPlaceFlowers.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} flowerbed
+ * @param {number} n
+ * @return {boolean}
+ */
+var canPlaceFlowers = function(flowerbed, n) {
+    if (n == 0) return true;
+    for (let i = 0; i < flowerbed.length; i++){
+        if ((flowerbed[i-1] == undefined || flowerbed[i-1] == 0) &&
+            flowerbed[i] == 0 &&
+            (flowerbed[i+1] == undefined || flowerbed[i+1] == 0)){
+            flowerbed[i] = 1;
+            n--;
+            if (n == 0) return true;
+        }
+    }
+    return false;
+};
+// https://leetcode.com/problems/can-place-flowers/
