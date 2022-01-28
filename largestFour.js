35c70ab4399f7973acffc19293f232db1463d4b1
Spencer Rauch <rauchs@gmail.com>
2022-01-28 10:32:58 -0800

    1/28

--- largestFour.js
+++ largestFour.js
@@ -1,4 +1,15 @@
+const keyString = "asdpa3252sadh0";
 
+const one = [4, 0, -1, 2];
+const resultOne = "5:0hdas2523apdsa";
+
+
+const two = [-1, 9, 0, -6, 1, 0];
+const resultTwo = "01:0hdas2523apdsa";
+
+const three = [-5, -9, 0, -6, 1, 0];
+const resultThree = "4-:0hdas2523apdsa";
+//returns the sum of the largest 4 values, appended to key string, which is then reversed
 //given to a fellow TA as an interview question
 
 function largestFour(arr) {
