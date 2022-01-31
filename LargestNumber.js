b52fa37c64a2a3df806c4e03da68e44f584b5b02
Spencer Rauch <rauchs@gmail.com>
2022-01-31 15:25:23 -0800

    largest

--- LargestNumber.js
+++ LargestNumber.js
@@ -5,7 +5,7 @@
  var largestNumber = function(nums) {
     if (nums.length == 1) return "" + nums[0]
     let sorted = nums.sort((a,b) => {
-        return (""+b + a)-(""+a + b)  ;
+        return (""+b + a)-(""+a + b);
     })
     let reduced = sorted.reduce((a,b) => (""+ a+ b)) 
     if (parseInt(reduced) == 0) return "0"
