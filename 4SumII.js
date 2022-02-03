82fb0030fa79e967c25596adf31b69b8efd0046c
Spencer Rauch <rauchs@gmail.com>
2022-02-03 08:12:12 -0800

    comments

--- 4SumII.js
+++ 4SumII.js
@@ -10,19 +10,20 @@
     let map = {}
     for (let one of nums1 ){             // create frequency table of all sums between nums1 and nums2
         for (let two of nums2){
-            let sum = one + two
-            if (!map[sum]){
-                map[sum] = 1
-            } else map[sum]++
+            let sum12 = one + two
+            if (!map[sum12]){
+                map[sum12] = 1
+            } else map[sum12]++
             
         }
     }
     
     for (let three of nums3){        // for each sum between nums3 and nums4
         for (let four of nums4){
-            let sum = three + four
-            if (map[-sum]){            // if the opposite sum exists in our freq table
-                count += map[-sum]   // we could make as many tuples as exist in that freq table with this sum
+            let sum34 = three + four
+            if (map[-sum34]){            // if the opposite sum exists in our freq table
+                                      // (i.e: the value that would make all four sum to zero)
+                count += map[-sum34]   // add the frequency of the opposite sum to our count, we can make this many tuples summing to 0 off this sum34
             }
             
         }
