b73ef5a13358f1b888384d9fa45c0a304a4d6073
Spencer Rauch <rauchs@gmail.com>
2022-02-12 09:42:41 -0800

    2/12

---
+++ WordLadder.js
@@ -0,0 +1,38 @@
+/**
+ * @param {string} beginWord
+ * @param {string} endWord
+ * @param {string[]} wordList
+ * @return {number}
+ */
+ var ladderLength = function (beginWord, endWord, wordList) {
+    let steps = 1
+    let queue = [beginWord]
+
+    while (queue.length) {
+        let nextQ = []
+        for (let word of queue) {
+            if (word == endWord) return steps
+            for (let i = 0; i < word.length; i++) {
+                for (let j = 0; j < 26; j++) {
+                    let newChar = String.fromCharCode(j + 97)
+                    if (newChar !== word[i]) {
+                        let newWord = word.slice(0, i) + newChar + word.slice(i + 1)
+                        let idx = wordList.indexOf(newWord)
+                        if (idx >= 0) {
+                            nextQ.push(newWord)
+                            delete wordList[idx]
+                            
+                        }
+                    }
+                }
+            }
+        }
+        queue = nextQ
+        steps++
+    }
+    return 0
+
+};
+
+//Time Limit Exceeded -- may could rewrite to use a set instead of an array
+//https://leetcode.com/problems/word-ladder/
