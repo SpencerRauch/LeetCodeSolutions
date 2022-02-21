9c7ae903585da2392cae482bae571e497ca9f6df
Spencer Rauch <rauchs@gmail.com>
2022-02-21 12:04:12 -0800

    2/21

--- MajorityElement.js
+++ MajorityElement.js
@@ -8,3 +8,36 @@
     
 }; 
 //https://leetcode.com/problems/majority-element
+
+//
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+ var majorityElement2 = function(nums) {
+    let count = 0;
+    let candidate = undefined;
+    
+    for(let num of nums){
+        if (count == 0){
+            candidate = num
+        }
+        count += (num == candidate ? 1 : -1)
+    }
+    return candidate;
+};
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+ var majorityElement3 = function(nums) {
+    let map = {}
+    let majority = Math.ceil(nums.length/2)
+    for ( let num of nums){
+        map[num] ? map[num]++ : map[num] = 1;
+        if (map[num] >= majority){
+            return num
+        }
+    }
+};
