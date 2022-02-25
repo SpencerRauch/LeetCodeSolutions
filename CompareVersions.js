ab8cc99012d9bceff969997abc968b0a0c3dc7c6
Spencer Rauch <rauchs@gmail.com>
2022-02-24 16:53:07 -0800

    2/24

---
+++ CompareVersions.js
@@ -0,0 +1,21 @@
+var compareVersion = function(version1, version2) {
+    version1 = version1.split('.');
+    version2 = version2.split('.');
+    
+    let index = 0;
+    
+    while (index < version1.length || index < version2.length) {
+        const num1 = version1[index] ? parseInt(version1[index]) : 0;
+        const num2 = version2[index] ? parseInt(version2[index]) : 0;
+        
+        if (num1 < num2) {
+            return -1;
+        } else if (num1 > num2) {
+            return 1;
+        } else {
+            index++;
+        }
+    }
+    
+    return 0;
+};
