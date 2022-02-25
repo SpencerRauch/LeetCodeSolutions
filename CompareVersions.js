67003474f2e342c66980b8bdf0609e98a78327c2
Spencer Rauch <rauchs@gmail.com>
2022-02-25 08:38:30 -0800

    2/25

--- CompareVersions.js
+++ CompareVersions.js
@@ -19,4 +19,20 @@ var compareVersion = function(version1, version2) {
     return 0;
 };
 
+var compareVersion = function(version1, version2) {
+    let v1Array = version1.split(".");
+    let v2Array = version2.split(".");
+    
+    let length = Math.max(v1Array.length, v2Array.length);
+    
+    for (let i = 0; i < length; i++){
+        let currentV1 = v1Array[i] ? parseInt(v1Array[i]) : 0
+        let currentV2 = v2Array[i] ? parseInt(v2Array[i]) : 0
+        
+        if (currentV1 < currentV2) return -1
+        if (currentV1 > currentV2) return 1
+    }
+    return 0
+};
+
 // https://leetcode.com/problems/compare-version-numbers
