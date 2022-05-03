b08805e31ba2b178ad8bea8c0819a492fd31bbad
Spencer Rauch <rauchs@gmail.com>
2022-05-03 10:04:29 -0700

    5/3

--- bubblesort.js
+++ bubblesort.js
@@ -12,7 +12,7 @@
 */
 
 const numsOrdered = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-const numsRandomOrder = [9, 2, 5, 6, 4, 3, 7, 10, 1, 8];
+const numsRandomOrder = [9, 2, 5, 6, 4, 3, 7, 10, 1, 8, 38,37, 20 , 21, 35, 48, 98, 1000];
 const numsReversed = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1];
 const expected = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
@@ -25,17 +25,19 @@ const expected = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
  * @returns {Array<number>} The given nums after being sorted.
  */
 function bubbleSort(nums) {
-    let isSorted = false;
-
-    while(!isSorted){
-        isSorted = true
-        for (let i = 0; i < nums.length-1; i++){
-            if (nums[i] > nums[i+1]){
-                isSorted = false;
-                [nums[i], nums[i+1]] = [nums[i+1], nums[i]]
+    let isSorted = false; //assume array isn't sorted to start
+    let stop = 0 // this will keep track of how many values we have floated to the end
+    while(!isSorted){ // if our array isn't sorted, start bubbling
+        isSorted = true // will stay true if we don't do any swaps
+        for (let i = 0; i < nums.length-1-stop; i++){ // iterate through the array
+            if (nums[i] > nums[i+1]){ // if the next value is smaller than the current value
+                isSorted = false; // flag that we are still floating values, and therefore not done sorting
+                [nums[i], nums[i+1]] = [nums[i+1], nums[i]] // perform swap
             }
         }
+      stop++ // increase stop amount because we floated one value to it's correct position
     }
+
     return nums
 }
 
