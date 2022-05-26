d629d9e608a52e6bfe108a67493614bcd0c5197e
Spencer Rauch <rauchs@gmail.com>
2022-05-26 08:36:49 -0700

    second solution

--- HammingWeight.js
+++ HammingWeight.js
@@ -12,5 +12,11 @@
     
 };
 
+var hammingWeight2 = function(n) {
+
+    return n.toString(2).split("").reduce((a,b)=>(+a + +b))
+    
+};
+
 // https://leetcode.com/problems/number-of-1-bits/submissions/
 
