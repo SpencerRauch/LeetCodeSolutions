6ead512fbb7027962976f45f9f62871c4ff567e5
Spencer Rauch <rauchs@gmail.com>
2022-06-07 07:16:04 -0700

    more methods!

--- LinkedList.js
+++ LinkedList.js
@@ -45,6 +45,34 @@ class List
             this.PushBack(val)
         }
     }
+    PushFront(value)
+	{
+		let node = new Node(value)
+        node.next = this.head;
+        this.head = node;
+	}
+
+	/* Remove and return the first node in the list, */
+	/* but also set the head to be the next node */
+	PopFront()
+	{
+        if (this.head !== undefined) this.head = this.head.next
+	}
+
+	/* Calculate and return the average of all the node values in the list. */
+	Average()
+	{
+		let sum = 0;
+        let count = 0;
+        let runner = this.head;
+        if (!runner) return 0;
+        while(runner){
+            count++
+            sum += runner.value
+            runner = runner.next
+        }
+        return (sum / count);
+	}
 
     Iterate(fn)
     {
@@ -59,6 +87,7 @@ let test_data4=[111,222,333,444,555];
 
 /* Create our list */
 let list=new List();
+console.log(list.IsEmpty());
 /* Insert nodes into the list: */
 list.PushBack(test_data1);
 list.PushBack(test_data2);
@@ -66,7 +95,16 @@ list.PushBack(test_data3);
 /* Insert array of nodes into the list: */
 list.PushBackN(test_data4);
 /* Iterate our list, so we can see our node values */
+list.PushFront(78)
+list.PopFront()
 list.Iterate(value=>console.log(value));
+
+let averageTest = new List();
+averageTest.PushFront(10)
+averageTest.PushFront(20)
+averageTest.PushFront(30)
+console.log(averageTest.Average())
+
 /* Expected output: */
 /*
     123
