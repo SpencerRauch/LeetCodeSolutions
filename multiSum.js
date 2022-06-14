09245f5a73bb4b199c9fd30ca086416061662c3a
Spencer Rauch <rauchs@gmail.com>
2022-06-14 12:10:53 -0700

    fixed returns

--- multiSum.js
+++ multiSum.js
@@ -1,14 +1,14 @@
 //given an array of size N, check if possible to generate a sum that equals to the target using the elements of the array
 
 function multiSum(N,nArr, S) {
-    if (nArr.length == 0) return false
+    if (nArr.length == 0) return "NO"
     let total = nArr.reduce((a, b) => a + b);
-    if (total === S) return true;
+    if (total === S) return "YES";
     for (let i = 0; i < nArr.length; i++) {
-        if (total - nArr[i] === S) return true;
-        if (multiSum(N-1,nArr.slice(0, i).concat(nArr.slice(i + 1)), S)) return true;
+        if (total - nArr[i] === S) return "YES";
+        if (multiSum(N-1,nArr.slice(0, i).concat(nArr.slice(i + 1)), S) == "YES") return "YES";
     }
-    return false;
+    return "NO";
 }
 
 
