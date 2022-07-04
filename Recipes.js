f75e21c1e831a6b57224d66dd7ce3adc5f1edf02
SpencerRauch <rauchs@gmail.com>
2022-07-04 13:49:18 -0700

    recipe

--- Recipes.js
+++ Recipes.js
@@ -30,7 +30,17 @@ function cakes(recipe, available) {
     }
 }
 
+function cakes2(recipe, available){
+    let minCount = Number.MAX_SAFE_INTEGER;
+    for (let key in recipe){
+        if (!available[key]) return 0;
+        minCount = Math.min(minCount, Math.floor(available[key]/recipe[key]))
+    }
+    return minCount;
+
+}
+
 // must return 2
-console.log(cakes({flour: 500, sugar: 200, eggs: 1}, {flour: 1200, sugar: 1200, eggs: 5, milk: 200})); 
+console.log(cakes2({flour: 500, sugar: 200, eggs: 1}, {flour: 1200, sugar: 1200, eggs: 5, milk: 200})); 
 // must return 0
-console.log(cakes({apples: 3, flour: 300, sugar: 150, milk: 100, oil: 100}, {sugar: 500, flour: 2000, milk: 2000})); 
+console.log(cakes2({apples: 3, flour: 300, sugar: 150, milk: 100, oil: 100}, {sugar: 500, flour: 2000, milk: 2000})); 
