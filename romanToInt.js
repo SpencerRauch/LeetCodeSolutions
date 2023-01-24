60eb2103590d8aa57ca98afb90141a766eb6b64d
Spencer Rauch <rauchs@gmail.com>
2023-01-24 08:25:29 -0800

    better

--- romanToInt.js
+++ romanToInt.js
@@ -48,3 +48,26 @@ var romanToInt = function(s) {
     }
     return int;
 };
+
+function romanToInt2 (s){
+    const romans = {
+        "I":1,
+        "V":5,
+        "X":10,
+        "L":50,
+        "C":100,
+        "D":500,
+        "M":1000
+    }
+    let int = 0;
+    for (let i=0; i < s.length; i++){
+        if (romans[s[i]] < romans[s[i+1]]){
+            total += romans[s[i+1]] - romans[s[i]]
+            i++
+        } else {
+            total += romans[s[i]]
+        }
+    }
+    return int;
+}
+
