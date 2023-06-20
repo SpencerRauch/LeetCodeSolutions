6bdb4023ff05749ef9c4da72c77c884911d536bd
Spencer Rauch <rauchs@gmail.com>
2023-06-20 08:23:47 -0700

    algo

---
+++ KRadiusSubarrayAverages.js
@@ -0,0 +1,25 @@
+//https://leetcode.com/problems/k-radius-subarray-averages/description/
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number[]}
+ */
+var getAverages = function(nums, k) {
+    let averages = Array(nums.length).fill(-1)
+    let rangeSize =  k * 2 + 1
+    if (nums.length < rangeSize) return averages
+    let i = k;
+    let total = 0;
+    for (let j = 0; j < k *2 +1; j++){
+        total += nums[j]
+    }
+    console.log(total)
+    while (i + k < nums.length){
+        averages[i] = parseInt(total / rangeSize);
+        total -= nums[i-k]
+        total += nums[i+k+1]
+        i++
+    }
+    return averages
+};
+console.log(getAverages([7,4,3,9,1,8,5,2,6],3))
