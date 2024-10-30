23f989d0cab5f9ba1a4dc94354dc04670fdf689f
Spencer Rauch <rauchs@gmail.com>
2024-10-30 08:34:32 -0700

    early return

--- MaxNumVowelsSubstring.js
+++ MaxNumVowelsSubstring.js
@@ -24,3 +24,31 @@ var maxVowels = function (s, k) {
     }
     return max
 }
+
+/**
+ * @param {string} s
+ * @param {number} k
+ * @return {number}
+ */
+var maxVowels = function (s, k) {
+    const isVowel = { a: true, e: true, i: true, o: true, u: true };
+    let max = 0;
+    let count = 0;
+    for (let i = 0; i < k; i++){
+        if (isVowel[s[i]]){
+            count++
+        }
+    }
+    max = count;
+    for (let i = k; i < s.length; i++){
+        if (isVowel[s[i-k]]){
+            count--
+        }
+        if (isVowel[s[i]]){
+            count++
+            if( count == k) return k //early return sped this up considerably
+        }
+        max = Math.max(max, count)
+    }
+    return max
+}
