ce111b4f0a7150247b5bb1ed6450529a5043532c
Spencer Rauch <rauchs@gmail.com>
2024-10-31 09:53:56 -0700

    set

--- UniqueNumberOfOccurences.js
+++ UniqueNumberOfOccurences.js
@@ -5,7 +5,7 @@
  */
 var uniqueOccurrences = function(arr) {
     const map = new Map();
-    const set = new Set();
+    // const set = new Set();
     for (let num of arr){
         if (!map.has(num)){
             map.set(num, 1)
@@ -13,8 +13,9 @@ var uniqueOccurrences = function(arr) {
             map.set(num, map.get(num)+1)
         }
     }
-    for (const value of map.values()){
-        set.add(value)
-    }
+    // for (const value of map.values()){
+    //     set.add(value)
+    // }
+    const set = new Set([...map.values()])
     return set.size == map.size
 };
