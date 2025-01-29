11dced3481629c1c0ec0bf59fd399fa0c668546f
Spencer Rauch <rauchs@gmail.com>
2025-01-29 14:15:45 -0800

    algo

--- plusone.js
+++ plusone.js
@@ -22,3 +22,26 @@ var plusOne = function(digits) {
     return digits.reverse()
 
 };
+
+
+/**
+ * @param {number[]} digits
+ * @return {number[]}
+ */
+var plusOne = function(digits) {
+    let end = digits.length-1 // let end be the last index of the array of digits
+    digits[end] += 1 // increase the digit in the last index by one
+    
+    while (digits[end] > 9 ){ // loop runs while last digit greater than 9 (ie is 10 and needs to carry over)
+        digits[end] = 0; // set current digit to 0
+        end--; // move to next digit (in order to "carry the one")
+        if (end < 0){ // if end is less than zero, we need to add an index at front with a 1
+            return [1, ...digits] // makes a new array with 1 and then everything in digits
+        }
+        else { //otherwise
+            digits[end] += 1 // we carry the one
+        }
+    }
+    return digits
+
+};
