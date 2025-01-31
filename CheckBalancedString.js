b729af8d9b2d99172976697fba8795d0b3484143
Spencer Rauch <rauchs@gmail.com>
2025-01-31 10:13:34 -0800

    algo

---
+++ CheckBalancedString.js
@@ -0,0 +1,15 @@
+/**
+ * @param {string} num
+ * @return {boolean}
+ */
+var isBalanced = function(num) {
+    let x = 0;
+    let y = 0;
+    for (let i = 0; i < num.length; i+=2){
+        x += +num[i]
+        if (i+1 < num.length){
+            y += +num[i+1]
+        }
+    }
+    return x == y
+};
