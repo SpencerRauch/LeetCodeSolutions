5483c56a400547dcc3e71246dc69a79d3bac2de5
Spencer Rauch <rauchs@gmail.com>
2025-04-14 08:10:15 -0700

    count good trips

---
+++ CountGoodTriplets.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} arr
+ * @param {number} a
+ * @param {number} b
+ * @param {number} c
+ * @return {number}
+ */
+var countGoodTriplets = function(arr, a, b, c) {
+    let count = 0;
+    for (let i = 0; i < arr.length-2; i++){
+
+        for (let j = i+1; j < arr.length-1; j++){
+            if (Math.abs(arr[i]-arr[j]) > a) continue
+            for (let k = j + 1; k < arr.length; k++){
+                if (Math.abs(arr[j] - arr[k]) > b) continue
+                if (Math.abs(arr[i]-arr[k]) <=c ){
+                    count++
+                } 
+            }
+        }
+    }
+    return count
+};
