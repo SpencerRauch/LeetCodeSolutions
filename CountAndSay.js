6b838d8bab7753632aaa99e975b4a33956d7e9b7
Spencer Rauch <rauchs@gmail.com>
2025-04-18 09:46:18 -0700

    algo

---
+++ CountAndSay.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number} n
+ * @return {string}
+ */
+var countAndSay = function (n) {
+    let currentString = "1";
+    for (let i = 2; i <= n; i++) {
+        let nextString = "";
+        for (let j = 0, k = 0; j < currentString.length; j = k) {
+            while (
+                k < currentString.length &&
+                currentString.charAt(k) == currentString.charAt(j)
+            )
+                k++;
+            nextString += (k - j).toString() + currentString.charAt(j);
+        }
+        currentString = nextString;
+    }
+    return currentString;
+};
